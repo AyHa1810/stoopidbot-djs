8fd287ee9afc9f304fff1a468848fbd269ead988
Nex4922 <72197572+Nex2944@users.noreply.github.com>
2022-02-04 21:37:55 +0500

    Update index.js to v13
    Update ayha bot to v13

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 require('dotenv').config();
-const Discord = require("discord.js")
-const client = new Discord.Client()
+const { Client, Intents } = require("discord.js")
+const client = new client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
 let spamCtrl = require('./spamCtrl');
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on("message", msg => {
+client.on("messageCreate", msg => {
   if (msg.content === "*hell-o") {
     msg.reply("bruh shut the fk up bitch lmfao");
   }
 })
 
-client.on("message", msg => {
+client.on("messageCreate", msg => {
   if (msg.content === "*nigg-") {
     msg.reply("YOU CAN'T SAY THE FUKIN N- WORD BRUH!!!");
   }
 })
 
-client.on("message", msg => {
+client.on("messageCreate", msg => {
   if (msg.content === "*no") {
     msg.reply("Shut bitch!");
   }
 })
 
-client.on("message", msg => {
+client.on("messageCreate", msg => {
   if (msg.content === "*AyHa bot alib") {
     msg.reply("Yay! haha");
   }
 })
 
-client.on("message", msg => {
+client.on("messageCreate", msg => {
   if (msg.content === "*bruh-help") {
     msg.channel.send("bruh-help = dis menu\nhell-o = says something\nnigg- = also says something\nno = ALSO SAYS SOMETHING AHAHA\nAyHa bot alib = k idk i succ at help menu");
   }
