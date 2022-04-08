c42d2778b6b3541441d8896119ffd9ae056a8f13
George Henderson <ayhahenderson2001@gmail.com>
2022-04-08 15:02:40 +0530

    Major changes and update the bot version to v0.1-alpha

---
+++ commands/admin/changeNickname.js
@@ -0,0 +1,24 @@
+// changeNickname.js changes the nickname of the bot
+exports.run = async (client, message, args, level) => {
+    const nick = args[0];
+    if (!nick) return message.channel.send("bruh specify a nickname to set!")
+    message.guild.me.setNickname(nick);
+    return message.channel.send("Nickname changed to " + nick)
+};
+
+// command's default config
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    cooldown: "5 sec",
+    aliases: ["nickname", "botnick"],
+    permLevel: "Administrator"
+};
+
+// command's help texts
+exports.help = {
+    name: "changenickname",
+    category: "Admin",
+    description: "Changes the bot's nickname in the guild.",
+    usage: "botnick [new nickname]"
+};
